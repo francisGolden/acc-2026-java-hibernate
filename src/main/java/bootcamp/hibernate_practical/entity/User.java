2ffb37af17cb3f680edf11886f04da68863fba67
Francesco Doria <doria5@live.it>
2026-07-06 15:43:33 +0300

    user entity

---
+++ src/main/java/bootcamp/hibernate_practical/entity/User.java
@@ -0,0 +1,32 @@
+package bootcamp.hibernate_practical.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;g
+    private String password;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+}
---
+++ src/main/java/bootcamp/hibernate_practical/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package bootcamp.hibernate_practical.repository;
+
+public interface UserRepository {
+}
