61f23b4121acbc143adfe07d3b6325e973441967
Francesco Doria <doria5@live.it>
2026-07-06 13:56:36 +0300

    security config user details service

--- src/main/java/bootcamp/hibernate_practical/config/SecurityConfig.java
+++ src/main/java/bootcamp/hibernate_practical/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package bootcamp.hibernate_practical.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.http.HttpMethod.*;
@@ -14,6 +21,8 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -23,7 +32,31 @@ public class SecurityConfig {
                         .requestMatchers(POST, "/api/books").hasRole("ADMIN")
                         .requestMatchers(PUT, "/api/books/**").hasRole("ADMIN")
                         .requestMatchers(DELETE, "/api/books/**").hasRole("ADMIN")
-                        .anyRequest().denyAll()).httpBasic(Customizer.withDefaults());
+                        .anyRequest()
+                        .denyAll())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("adminCompromisedPassword"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(encoder.encode("userCompromisedPassword"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
