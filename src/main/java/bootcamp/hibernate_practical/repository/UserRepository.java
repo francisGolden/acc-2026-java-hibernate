35dada971ab9d98c79eda950143dcae77ef4a516
Francesco Doria <doria5@live.it>
2026-07-06 15:56:16 +0300

    user repository

--- src/main/java/bootcamp/hibernate_practical/repository/UserRepository.java
+++ src/main/java/bootcamp/hibernate_practical/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package bootcamp.hibernate_practical.repository;
 
-public interface UserRepository {
+import bootcamp.hibernate_practical.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
