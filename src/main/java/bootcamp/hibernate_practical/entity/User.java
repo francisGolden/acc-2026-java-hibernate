b54c44dfbb4926abb7d3b6dd70e3b3eec6a1e0bd
Francesco Doria <doria5@live.it>
2026-07-06 15:56:02 +0300

    user entity

--- src/main/java/bootcamp/hibernate_practical/entity/User.java
+++ src/main/java/bootcamp/hibernate_practical/entity/User.java
@@ -1,32 +1,67 @@
 package bootcamp.hibernate_practical.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
-    private String username;g
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private String email;
+
     @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
     private LocalDate createdAt;
 
     @UpdateTimestamp
+    @Column(name = "updated_at")
     private LocalDate updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
